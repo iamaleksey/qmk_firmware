5a5ec1e69e28b967c2762ff0721baef3959dd19c
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-05-09 23:08:24 +0100

    clean up music, enable encoder

--- keyboards/preonic/keymaps/iamaleksey/config.h
+++ keyboards/preonic/keymaps/iamaleksey/config.h
@@ -1,34 +1,15 @@
 #pragma once
 
-#ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
+#define ENCODER_RESOLUTION 2
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
-#endif
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 180
 
-#define MUSIC_MASK (keycode != KC_NO)
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
 
-/*
- * MIDI options
- */
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
 
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
-#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
