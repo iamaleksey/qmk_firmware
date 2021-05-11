b2f3511dff885953ccbd69c76c8ba243e6bed365
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-05-11 21:12:16 +0100

    fix encoders and configure mod tap

--- keyboards/kyria/keymaps/iamaleksey/config.h
+++ keyboards/kyria/keymaps/iamaleksey/config.h
@@ -28,6 +28,20 @@
   #define RGBLIGHT_LIMIT_VAL 150
 #endif
 
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+// flip default directions
+#define ENCODER_DIRECTION_FLIP
+
+// To prevent skipping steps
+#define ENCODER_RESOLUTION 2
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 150
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
