16566dbe05697ce30a5695e2f3025fd33f68e272
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-05-24 22:03:11 +0100

    replace ctrl with alt on wasd layer

--- keyboards/kyria/keymaps/iamaleksey/keymap.c
+++ keyboards/kyria/keymaps/iamaleksey/keymap.c
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | Shift  |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | .  > | /  ? | Shift  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | Ctrl | Lower| Space| Enter|  | Enter| Bksp | Raise| Ctrl |      |
+ *                        |      |  Alt | Lower| Space| Enter|  | Enter| Bksp | Raise| Alt  |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,    KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
       KC_GESC,   KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LSFT,   KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    _______, _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                 _______, KC_LCTL, LOWER,   KC_SPC,  KC_ENT,  KC_ENT,  KC_BSPC, RAISE,   KC_RCTL, _______
+                                 _______, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  KC_ENT,  KC_BSPC, RAISE,   KC_RALT, _______
     ),
 
 /*
