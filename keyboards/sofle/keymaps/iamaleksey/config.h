5582b4a4c58dc245b221a9fb3f03940370452028
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-06-06 01:13:40 +0100

    sofle: modify for Benedict

--- keyboards/sofle/keymaps/iamaleksey/config.h
+++ keyboards/sofle/keymaps/iamaleksey/config.h
@@ -10,7 +10,7 @@ for more options.
 #define ENCODER_RESOLUTIONS { 4, 2 }
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 180 
+#define TAPPING_TERM 175
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
