91c168872eecbeba1f1460d8db205ea988099bd1
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-06-16 21:10:06 +0100

    planck: prettify

--- keyboards/planck/keymaps/iamaleksey/keymap.c
+++ keyboards/planck/keymaps/iamaleksey/keymap.c
@@ -1,58 +1,4 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include QMK_KEYBOARD_H
-
-enum layers {
-  _COLEMAK,
-  _QWERTY,
-  _WASD,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
-
-// Home row mods (QWERTY)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_S LOPT_T(KC_S)
-#define LCMD_D LCMD_T(KC_D)
-#define SHFT_F LSFT_T(KC_F)
-#define RSFT_J RSFT_T(KC_J)
-#define RCMD_K RCMD_T(KC_K)
-#define ROPT_L ROPT_T(KC_L)
-#define CTL_SCLN RCTL_T(KC_SCLN)
-
-// Home row mods (Colemak-DHm)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_R LOPT_T(KC_R)
-#define LCMD_S LCMD_T(KC_S)
-#define SHFT_T LSFT_T(KC_T)
-#define RSFT_N RSFT_T(KC_N)
-#define RCMD_E RCMD_T(KC_E)
-#define ROPT_I LOPT_T(KC_I)
-#define RCTL_O RCTL_T(KC_O)
-
-// Layer switch / mod taps
-#define LWR_TAB LT(_LOWER, KC_TAB)
-#define RSE_ENT LT(_RAISE, KC_ENT)
-#define QWERTY  DF(_QWERTY)
-#define COLEMAK DF(_COLEMAK)
-#define WASD    DF(_WASD)
-#define LOWER   MO(_LOWER)
-#define RAISE   MO(_RAISE)
+#include "keymap.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -60,7 +6,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  | ;  : | \  | |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc ~| A/Ctl| R/Opt| S/Cmd| T/Sft|   G  |   M  | N/Sft| E/Cmd| I/Opt| O/Ctl| '  " |
+ * | Esc `| A/Ctl| R/Opt| S/Cmd| T/Sft|   G  |   M  | N/Sft| E/Cmd| I/Opt| O/Ctl| '  " |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |   Z  |   X  |   C  |   D  |   V  |   K  |   H  | ,  < | .  > | /  ? |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -78,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | \  | |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc ~| A/Ctl| S/Opt| D/Cmd| F/Sft|   G  |   H  | J/Sft| K/Cmd| L/Opt|;:/Ctl| '  " |
+ * | Esc `| A/Ctl| S/Opt| D/Cmd| F/Sft|   G  |   H  | J/Sft| K/Cmd| L/Opt|;:/Ctl| '  " |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,  < | .  > | /  ? |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -96,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | \  | |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc ~|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ;  : | '  " |
+ * | Esc `|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ;  : | '  " |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,  < | .  > | /  ? | Ent  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -104,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_WASD] = LAYOUT_ortho_4x12(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    XXXXXXX, KC_LCTL, KC_LOPT, KC_LCMD, LOWER,   KC_SPC,  KC_BSPC, RAISE,   KC_RCMD, KC_ROPT, KC_RCTL, XXXXXXX
+    KC_TAB,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_A,    KC_S,    KC_D,    KC_F,    _______, _______, KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
+    KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_ENT,
+    XXXXXXX, KC_LCTL, KC_LOPT, KC_LCMD, LOWER,   _______, _______, RAISE,   KC_RCMD, KC_ROPT, KC_RCTL, XXXXXXX
 ),
 
 /* Lower
@@ -118,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   $  |   @  |   {  |   }  |   &  |   `  |   [  |   ]  |   %  |   ^  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      | Space| Bspc |Adjust|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    _______, KC_EXLM, KC_PLUS, KC_LPRN, KC_RPRN, KC_HASH, KC_PIPE, KC_MINS, KC_EQL,  KC_UNDS, KC_ASTR, _______,
-    _______, KC_DLR,  KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR, KC_GRV,  KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+    XXXXXXX, KC_EXLM, KC_PLUS, KC_LPRN, KC_RPRN, KC_HASH, KC_PIPE, KC_MINS, KC_EQL,  KC_UNDS, KC_ASTR, XXXXXXX,
+    XXXXXXX, KC_DLR,  KC_AT,   KC_LCBR, KC_RCBR, KC_AMPR, KC_GRV,  KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  KC_BSPC, RAISE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Raise
@@ -136,21 +82,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      | Vol- | Mute | Vol+ |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |Adjust| Space| Bspc |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-    _______, _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, LOWER,   KC_SPC,  KC_BSPC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
  * |      | F1   | F2   | F3   | F4   | F5   | F6   | F7   | F8   | F9   | F10  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Togl | Sat+ | Hue+ | Bri+ | Mode+|      |QWERTY|COLEMK| WASD | F11  |      |
+ * |      | Togl | Sat+ | Hue+ | Bri+ | Mode+|      |COLEMK|QWERTY| WASD | F11  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      | Togl | Sat- | Hue- | Bri- | Mode-|      |      |      |      | F12  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -158,10 +104,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_ortho_4x12(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-    _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, _______, QWERTY,  COLEMAK, WASD,    KC_F11,  _______,
-    _______, RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______, _______, _______, _______, KC_F12,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+    XXXXXXX, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, XXXXXXX, COLEMAK, QWERTY,  WASD,    KC_F11,  XXXXXXX,
+    XXXXXXX, RGB_TOG, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F12,  XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
 
 };
