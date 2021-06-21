229e2e03a0b336bbb03e588ad2f37e086d41ccb2
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-06-21 22:15:09 +0100

    planck: CAPS_WRD and persistent default layers

--- keyboards/planck/keymaps/iamaleksey/keymap.c
+++ keyboards/planck/keymaps/iamaleksey/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc `| A/Ctl| R/Opt| S/Cmd| T/Sft|   G  |   M  | N/Sft| E/Cmd| I/Opt| O/Ctl| '  " |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   D  |   V  |   K  |   H  | ,  < | .  > | /  ? |      |
+ * |      |   Z  |   X  |   C  |   D  |   V  |   K  |   H  | ,  < | .  > | /  ? |CpsWrd|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |LwrTab| Space| Bspc |RseEnt|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT_ortho_4x12(
     XXXXXXX, Q_ESC,   KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
     KC_GESC, LCTL_A,  LOPT_R,  LCMD_S,  SHFT_T,  KC_G,    KC_M,    RSFT_N,  RCMD_E,  ROPT_I,  RCTL_O,  KC_QUOT,
-    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, CAPS_WRD,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LWR_TAB, KC_SPC,  KC_BSPC, RSE_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Esc `| A/Ctl| S/Opt| D/Cmd| F/Sft|   G  |   H  | J/Sft| K/Cmd| L/Opt|;:/Ctl| '  " |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,  < | .  > | /  ? |      |
+ * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  | ,  < | .  > | /  ? |CpsWrd|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |LwrTab| Space| Bspc |RseEnt|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ortho_4x12(
     XXXXXXX, Q_ESC,   KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_GESC, LCTL_A,  LOPT_S,  LCMD_D,  SHFT_F,  KC_G,    KC_H,    RSFT_J,  RCMD_K,  ROPT_L,  CTL_SCLN,KC_QUOT,
-    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+    XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, CAPS_WRD,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LWR_TAB, KC_SPC,  KC_BSPC, RSE_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
@@ -119,3 +119,94 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 qk_tap_dance_action_t tap_dance_actions[] = {
     [_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC)
 };
+
+// CAPS_WORD: A "smart" Caps Lock key that only capitalizes the next identifier you type
+// and then toggles off Caps Lock automatically when you're done.
+void caps_word_enable(void) {
+    caps_word_on = true;
+    if (!(host_keyboard_led_state().caps_lock)) {
+        tap_code(KC_CAPS);
+    }
+}
+
+void caps_word_disable(void) {
+    caps_word_on = false;
+    if (host_keyboard_led_state().caps_lock) {
+        tap_code(KC_CAPS);
+    }
+}
+
+// Used to extract the basic tapping keycode from a dual-role key.
+// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
+#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
+
+void process_caps_word(uint16_t keycode, const keyrecord_t *record) {
+    // Update caps word state
+    if (caps_word_on) {
+        switch (keycode) {
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                // Earlier return if this has not been considered tapped yet
+                if (record->tap.count == 0) { return; }
+                // Get the base tapping keycode of a mod- or layer-tap key
+                keycode = GET_TAP_KC(keycode);
+                break;
+            default:
+                break;
+        }
+
+        switch (keycode) {
+            // Keycodes to shift
+            case KC_A ... KC_Z:
+                if (record->event.pressed) {
+                    caps_word_enable();
+                }
+            // Keycodes that enable caps word but shouldn't get shifted
+            case KC_MINS:
+            case KC_BSPC:
+            case KC_UNDS:
+            case KC_PIPE:
+            case CAPS_WRD:
+                // If chording mods, disable caps word
+                if (record->event.pressed && (get_mods() != MOD_LSFT) && (get_mods() != 0)) {
+                    caps_word_disable();
+                }
+                break;
+            default:
+                // Any other keycode should automatically disable caps
+                if (record->event.pressed) {
+                    caps_word_disable();
+                }
+                break;
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    process_caps_word(keycode, record);
+
+    switch (keycode) {
+    case CAPS_WRD:
+	// Toggle `caps_word_on`
+	if (record->event.pressed) {
+	    if (caps_word_on) {
+		caps_word_disable();
+	    } else {
+		caps_word_enable();
+	    }
+	    return false;
+	}
+	break;
+    case COLEMAK:
+	set_single_persistent_default_layer(_COLEMAK);
+	return false;
+    case QWERTY:
+	set_single_persistent_default_layer(_QWERTY);
+	return false;
+    case WASD:
+	set_single_persistent_default_layer(_WASD);
+	return false;
+    }
+
+    return true;
+}
