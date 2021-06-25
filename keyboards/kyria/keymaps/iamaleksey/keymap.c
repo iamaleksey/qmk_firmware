9e9a5bc945be75ad3163dd356f45049299ca0a8e
Aleksey Yeschenko <aleksey@apache.org>
2021-06-25 21:50:45 +0100

    kyria: implement caps word, qq, and dotdot tapdances

--- keyboards/kyria/keymaps/iamaleksey/keymap.c
+++ keyboards/kyria/keymaps/iamaleksey/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                              |------+------+------+------+------+------|
  * | Esc ~| A/Ctl| R/Opt| S/Cmd| T/Sft|   G  |                              |   M  | N/Sft| E/Cmd| I/Opt| O/Ctl| '  " |
  * |------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   D  |   V  |      |      |  |      |      |   K  |   H  | ,  < | .  > | /  ? |      |
+ * |      |   Z  |   X  |   C  |   D  |   V  |      |      |  |      |      |   K  |   H  | ,  < | .  > | /  ? |CpsWrd|
  * `--------------------+------+------+------+------+------|  |------+------+------+------+------+--------------------'
  *                      |      |      | Tab/ | Space| Prev |  | Next | Bspc | Entr/|      |      |
  *                      |      |      | Lower|      | tab  |  | tab  |      | Raise|      |      |
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
       XXXXXXX, KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSLS,
       KC_GESC, A_LCTL , R_LOPT , S_LCMD , T_LSFT , KC_G   ,                                     KC_M   , N_RSFT , E_RCMD , I_ROPT , O_RCTL , KC_QUOT,
-      XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
+      XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH,CAPS_WRD,
                                  XXXXXXX, XXXXXXX, LWR_TAB, KC_SPC , PRV_TAB, NXT_TAB, KC_BSPC, RSE_ENT, XXXXXXX, XXXXXXX
     ),
 
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                              |------+------+------+------+------+------|
  * | Esc ~| A/Ctl| S/Opt| D/Cmd| F/Sft|   G  |                              |   H  | J/Sft| K/Cmd| L/Opt|;:/Ctl| '  " |
  * |------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | .  > | /  ? |      |
+ * |      |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | .  > | /  ? |CpsWrd|
  * `--------------------+------+------+------+------+------|  |------+------+------+------+------+--------------------'
  *                      |      |      | Tab/ | Space| Prev |  | Next | Bspc | Entr/|      |      |
  *                      |      |      | Lower|      | tab  |  | tab  |      | Raise|      |      |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       XXXXXXX, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
       KC_GESC, A_LCTL , S_LOPT , D_LCMD , F_LSFT , KC_G   ,                                     KC_H   , J_RSFT , K_RCMD , L_ROPT ,SCN_RCLT, KC_QUOT,
-      XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
+      XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,CAPS_WRD,
                                  XXXXXXX, XXXXXXX, LWR_TAB, KC_SPC , PRV_TAB, NXT_TAB, KC_BSPC, RSE_ENT, XXXXXXX, XXXXXXX
     ),
 
@@ -180,8 +180,83 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 }
 #endif
 
+// CAPS_WORD: A "smart" Caps Lock key that only capitalizes the next identifier you type
+// and then toggles off Caps Lock automatically when you're done.
+void caps_word_enable(void) {
+    caps_word_on = true;
+    if (!(host_keyboard_led_state().caps_lock)) {
+        tap_code(KC_CAPS);
+    }
+}
+
+void caps_word_disable(void) {
+    caps_word_on = false;
+    if (host_keyboard_led_state().caps_lock) {
+        tap_code(KC_CAPS);
+    }
+}
+
+// Used to extract the basic tapping keycode from a dual-role key.
+// Example: GET_TAP_KC(MT(MOD_RSFT, KC_E)) == KC_E
+#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
+
+void process_caps_word(uint16_t keycode, const keyrecord_t *record) {
+    // Update caps word state
+    if (caps_word_on) {
+        switch (keycode) {
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                // Earlier return if this has not been considered tapped yet
+                if (record->tap.count == 0) { return; }
+                // Get the base tapping keycode of a mod- or layer-tap key
+                keycode = GET_TAP_KC(keycode);
+                break;
+            default:
+                break;
+        }
+
+        switch (keycode) {
+            // Keycodes to shift
+            case KC_A ... KC_Z:
+                if (record->event.pressed) {
+                    caps_word_enable();
+                }
+            // Keycodes that enable caps word but shouldn't get shifted
+            case KC_MINS:
+            case KC_BSPC:
+            case KC_UNDS:
+            case KC_PIPE:
+            case CAPS_WRD:
+                // If chording mods, disable caps word
+                if (record->event.pressed && (get_mods() != MOD_LSFT) && (get_mods() != 0)) {
+                    caps_word_disable();
+                }
+                break;
+            default:
+                // Any other keycode should automatically disable caps
+                if (record->event.pressed) {
+                    caps_word_disable();
+                }
+                break;
+        }
+    }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    process_caps_word(keycode, record);
+
     switch (keycode) {
+    case CAPS_WRD:
+        // Toggle `caps_word_on`
+        if (record->event.pressed) {
+            if (caps_word_on) {
+                caps_word_disable();
+            } else {
+                caps_word_enable();
+            }
+            return false;
+        }
+        break;
     case COLEMAK:
         set_single_persistent_default_layer(_COLEMAK);
         return false;
@@ -195,3 +270,51 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     return true;
 }
+
+// Tap dances:
+// 1. Double Q to Escape
+// 2. Double dot to issue ". <one-shot-shift>" i.e. dot, space and capitalize next letter
+
+void sentence_end(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+    // Double tapping TD_DOT produces
+    // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
+    // This helps to quickly end a sentence and begin another one
+    // without having to hit shift.
+    case 2:
+        /* Check that Shift is inactive */
+        if (!(get_mods() & MOD_MASK_SHIFT)) {
+            tap_code(KC_SPC);
+            /* Internal code of OSM(MOD_LSFT) */
+            add_oneshot_mods(MOD_BIT(KC_LSHIFT));
+
+        } else {
+            // send ">" (KC_DOT + shift → ">")
+            tap_code(KC_DOT);
+        }
+        break;
+
+    // Since `sentence_end` is called on each tap
+    // and not at the end of the tapping term,
+    // the third tap needs to cancel the effects
+    // of the double tap in order to get the expected
+    // three dots ellipsis.
+    case 3:
+        // remove the added space of the double tap case
+        tap_code(KC_BSPC);
+        // replace the space with a second dot
+        tap_code(KC_DOT);
+        // tap the third dot
+        tap_code(KC_DOT);
+        break;
+
+    // send KC_DOT on every normal tap of TD_DOT
+    default:
+        tap_code(KC_DOT);
+    }
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [_TD_Q]   = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+    [_TD_DOT] = ACTION_TAP_DANCE_FN_ADVANCED(sentence_end, NULL, NULL)
+};
