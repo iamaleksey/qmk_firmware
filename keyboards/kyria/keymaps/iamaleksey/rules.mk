7b051424180547153bb9b3f470db327c25825073
Aleksey Yeschenko <aleksey@apache.org>
2021-06-25 21:51:34 +0100

    kyria: disable oled driver

--- keyboards/kyria/keymaps/iamaleksey/rules.mk
+++ keyboards/kyria/keymaps/iamaleksey/rules.mk
@@ -1,3 +1,3 @@
-OLED_DRIVER_ENABLE = yes  # enables the use of OLED displays
+OLED_DRIVER_ENABLE = no   # enables the use of OLED displays
 ENCODER_ENABLE     = yes  # enables the use of one or more encoders
 RGBLIGHT_ENABLE    = yes  # enables keyboard RGB underglow
