d53bc202237c510ffe9f76b1c9bcbd05612ab7b6
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-08-12 20:07:54 +0100

    alt: enable dot-dot tap-dance

--- keyboards/massdrop/alt/keymaps/iamaleksey/keymap.c
+++ keyboards/massdrop/alt/keymaps/iamaleksey/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         CAPS_WRD,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, TD_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
@@ -178,3 +178,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; //Process all other keycodes normally
     }
 }
+
+// Double dot to issue ". <one-shot-shift>" i.e. dot, space and capitalize next letter
+void sentence_end(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+    // Double tapping TD_DOT produces
+    // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
+    // This helps to quickly end a sentence and begin another one
+    // without having to hit shift.
+    case 2:
+        /* Check that Shift is inactive */
+        if (!(get_mods() & MOD_MASK_SHIFT)) {
+            tap_code(KC_SPC);
+            /* Internal code of OSM(MOD_LSFT) */
+            add_oneshot_mods(MOD_BIT(KC_LSHIFT));
+
+        } else {
+            // send ">" (KC_DOT + shift → ">")
+            tap_code(KC_DOT);
+        }
+        break;
+
+    // Since `sentence_end` is called on each tap
+    // and not at the end of the tapping term,
+    // the third tap needs to cancel the effects
+    // of the double tap in order to get the expected
+    // three dots ellipsis.
+    case 3:
+        // remove the added space of the double tap case
+        tap_code(KC_BSPC);
+        // replace the space with a second dot
+        tap_code(KC_DOT);
+        // tap the third dot
+        tap_code(KC_DOT);
+        break;
+
+    // send KC_DOT on every normal tap of TD_DOT
+    default:
+        tap_code(KC_DOT);
+    }
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [_TD_DOT] = ACTION_TAP_DANCE_FN_ADVANCED(sentence_end, NULL, NULL)
+};
