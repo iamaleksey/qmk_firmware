e65f16e914374a1d07644254265b259a04de17f9
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-08-19 22:14:37 +0100

    jules: simplify/unify

--- keyboards/wilba_tech/wt65_f/keymaps/iamaleksey/keymap.h
+++ keyboards/wilba_tech/wt65_f/keymaps/iamaleksey/keymap.h
@@ -1,35 +1,21 @@
 #include QMK_KEYBOARD_H
 
-enum layers { _COLEMAK, _QWERTY, _SYMBOL, _EXTEND, _ADJUST };
+enum layers { _COLEMAK, _SYMBOL, _EXTEND, _ADJUST };
 
-// Layer switching / mod taps
-#define COLEMAK DF(_COLEMAK)
-#define QWERTY  DF(_QWERTY)
-#define EXTEND  MO(_EXTEND)
-#define SYMBOL  MO(_SYMBOL)
-#define ADJUST  MO(_ADJUST)
+// Layer switching
 #define TAB_SYM LT(_SYMBOL, KC_TAB)
+#define ESC_EXT LT(_EXTEND, KC_BSPC)
 #define BSP_EXT LT(_EXTEND, KC_BSPC)
 
-// Home row mods (Colemak-DHm)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_R LOPT_T(KC_R)
-#define LCMD_S LCMD_T(KC_S)
-#define SHFT_T LSFT_T(KC_T)
-#define RSFT_N RSFT_T(KC_N)
-#define RCMD_E RCMD_T(KC_E)
-#define ROPT_I LOPT_T(KC_I)
-#define RCTL_O RCTL_T(KC_O)
-
-// Home row mods (QWERTY)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_S LOPT_T(KC_S)
-#define LCMD_D LCMD_T(KC_D)
-#define SHFT_F LSFT_T(KC_F)
-#define RSFT_J RSFT_T(KC_J)
-#define RCMD_K RCMD_T(KC_K)
-#define ROPT_L ROPT_T(KC_L)
-#define CTL_SCLN RCTL_T(KC_SCLN)
+// Home row mods
+#define A_LCTL LCTL_T(KC_A)
+#define R_LOPT LOPT_T(KC_R)
+#define S_LCMD LCMD_T(KC_S)
+#define T_LSFT LSFT_T(KC_T)
+#define N_RSFT RSFT_T(KC_N)
+#define E_RCMD RCMD_T(KC_E)
+#define I_ROPT ROPT_T(KC_I)
+#define O_RCTL RCTL_T(KC_O)
 
 // Text navigation / manipulation
 #define LNE_BEG LCMD(KC_LEFT)
