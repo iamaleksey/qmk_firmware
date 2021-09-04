ea3d4063ee966872c8dd284eab380a5eaac0991c
Aleksey Yeschenko <aleksey@yeschenko.com>
2021-09-04 23:35:11 +0100

    planck: unify

--- keyboards/planck/keymaps/iamaleksey/keymap.h
+++ keyboards/planck/keymaps/iamaleksey/keymap.h
@@ -1,40 +1,47 @@
 #include QMK_KEYBOARD_H
 
-enum layers { _COLEMAK, _QWERTY, _WASD, _LOWER, _RAISE, _ADJUST };
-#define LWR_TAB LT(_LOWER, KC_TAB)
-#define RSE_ENT LT(_RAISE, KC_ENT)
+enum layers { _COLEMAK, _WASD, _LOWER, _RAISE, _ADJUST };
+
+enum keycodes { CAPS_WRD = SAFE_RANGE };
+
+// Layer switching
+#define COLEMAK DF(_COLEMAK)
+#define WASD    DF(_WASD)
 #define LOWER   MO(_LOWER)
 #define RAISE   MO(_RAISE)
+#define TAB_LWR LT(_LOWER, KC_TAB)
+#define ESC_RSE LT(_RAISE, KC_ESC)
+#define ENT_RSE LT(_RAISE, KC_ENT)
 
-// Home row mods (Colemak-DHm)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_R LOPT_T(KC_R)
-#define LCMD_S LCMD_T(KC_S)
-#define SHFT_T LSFT_T(KC_T)
-#define RSFT_N RSFT_T(KC_N)
-#define RCMD_E RCMD_T(KC_E)
-#define ROPT_I LOPT_T(KC_I)
-#define RCTL_O RCTL_T(KC_O)
+// Home row mods
+#define A_LCTL LCTL_T(KC_A)
+#define R_LOPT LOPT_T(KC_R)
+#define S_LCMD LCMD_T(KC_S)
+#define T_LSFT LSFT_T(KC_T)
+#define N_RSFT RSFT_T(KC_N)
+#define E_RCMD RCMD_T(KC_E)
+#define I_ROPT ROPT_T(KC_I)
+#define O_RCTL RCTL_T(KC_O)
 
-// Home row mods (QWERTY)
-#define LCTL_A LCTL_T(KC_A)
-#define LOPT_S LOPT_T(KC_S)
-#define LCMD_D LCMD_T(KC_D)
-#define SHFT_F LSFT_T(KC_F)
-#define RSFT_J RSFT_T(KC_J)
-#define RCMD_K RCMD_T(KC_K)
-#define ROPT_L ROPT_T(KC_L)
-#define CTL_SCLN RCTL_T(KC_SCLN)
+// Text navigation / manipulation
+#define LNE_BEG LCMD(KC_LEFT)
+#define LNE_END LCMD(KC_RGHT)
+#define DEL_LNE LCMD(KC_BSPC)
+#define PRV_WRD LOPT(KC_LEFT)
+#define NXT_WRD LOPT(KC_RGHT)
 
-// Custom key codes
-enum key_codes { CAPS_WRD = SAFE_RANGE, COLEMAK, QWERTY, WASD };
+// Undo / redo / cut / copy / paste
+#define UNDO  LCMD(KC_Z)
+#define REDO  LSFT(UNDO)
+#define CUT   LCMD(KC_X)
+#define COPY  LCMD(KC_C)
+#define PASTE LCMD(KC_V)
 
-// CAPS_WRD
+// Caps word
 bool caps_word_on;
 void caps_word_enable(void);
 void caps_word_disable(void);
 
 // Tap dances
-enum dances { _TD_Q, _TD_DOT };
-#define TD_Q   TD(_TD_Q)
+enum dances { _TD_DOT };
 #define TD_DOT TD(_TD_DOT)
