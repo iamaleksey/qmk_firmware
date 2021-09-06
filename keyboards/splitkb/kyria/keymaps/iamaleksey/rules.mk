994f2d675a42b202931e854ae8dafb7d741361e9
Aleksey Yeschenko <aleksey@apache.org>
2021-09-06 17:07:27 +0100

    kyria: cut firmware size

--- keyboards/splitkb/kyria/keymaps/iamaleksey/rules.mk
+++ keyboards/splitkb/kyria/keymaps/iamaleksey/rules.mk
@@ -1,3 +1,4 @@
+MOUSEKEY_ENABLE    = no  # disable mouse key
 OLED_DRIVER_ENABLE = no  # disables the use of OLED displays
 ENCODER_ENABLE     = yes # enables the use of one or more encoders
 RGBLIGHT_ENABLE    = yes # enables keyboard RGB underglow
