47c394e67fe0033350052b0bf2591323e0863ea2
Aleksey Yeschenko <aleksey@apache.org>
2021-09-09 14:08:43 +0100

    space65: unify

--- keyboards/gray_studio/space65/keymaps/iamaleksey/keymap.c
+++ keyboards/gray_studio/space65/keymaps/iamaleksey/keymap.c
@@ -2,34 +2,33 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_COLEMAK] = LAYOUT_65_ansi_blocker(
-	KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,          KC_HOME,
-	KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-	ESC_EXT, A_LCTL , R_LOPT , S_LCMD , T_LSFT , KC_G   , KC_M   , N_RSFT , E_RCMD , I_ROPT , O_RCTL , KC_QUOT, KC_ENT ,                   KC_PGDN,
-	KC_LSFT, KC_X   , KC_C   , KC_D   , KC_V   , KC_Z   , KC_K   , KC_H   , KC_COMM, TD_DOT , KC_SLSH,CAPS_WRD,                   KC_UP  , KC_END ,
-	KC_LCTL, KC_LOPT, TAB_SYM,                            KC_SPC ,                            BSP_EXT, KC_ROPT,          KC_LEFT, KC_DOWN, KC_RGHT),
+[_COLEMAK] = LAYOUT_65_ansi_blocker(
+    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,          KC_HOME,
+    KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
+    ESC_EXT, A_LCTL , R_LOPT , S_LCMD , T_LSFT , KC_G   , KC_M   , N_RSFT , E_RCMD , I_ROPT , O_RCTL , KC_QUOT, KC_ENT ,                   KC_PGDN,
+    KC_LSFT, KC_X   , KC_C   , KC_D   , KC_V   , KC_Z   , KC_K   , KC_H   , KC_COMM, TD_DOT , KC_SLSH,CAPS_WRD,                   KC_UP  , KC_END ,
+    KC_LCTL, KC_LOPT, TAB_SYM,                            KC_SPC ,                            BSP_EXT, KC_ROPT,          KC_LEFT, KC_DOWN, KC_RGHT),
 
-    [_SYMBOL] = LAYOUT_65_ansi_blocker(
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-	_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, _______, _______,          _______,
-	KC_TILD, KC_EXLM, KC_PLUS, KC_LPRN, KC_RPRN, KC_AMPR, KC_PIPE, KC_MINS, KC_EQL , KC_UNDS, KC_ASTR, _______, _______,                   _______,
-	_______, KC_AT  , KC_LCBR, KC_RCBR, KC_HASH, KC_DLR , KC_GRV , KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, _______,                   _______, _______,
-	_______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______),
+[_SYMBOL] = LAYOUT_65_ansi_blocker(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, _______, _______,          _______,
+    KC_TILD, KC_EXLM, KC_PLUS, KC_LPRN, KC_RPRN, KC_AMPR, KC_PIPE, KC_MINS, KC_EQL , KC_UNDS, KC_ASTR, _______, _______,                   _______,
+    _______, KC_AT  , KC_LCBR, KC_RCBR, KC_HASH, KC_DLR , KC_GRV , KC_LBRC, KC_RBRC, KC_PERC, KC_CIRC, _______,                   _______, _______,
+    _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______),
 
-    [_EXTEND] = LAYOUT_65_ansi_blocker(
-	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-	_______, KC_INS , KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_PGUP, PRV_WRD, KC_UP  , NXT_WRD, DEL_LNE, KC_BSPC, _______, _______,          _______,
-	_______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , KC_BSPC, _______,                   _______,
-	_______, CUT    , COPY   , PASTE  , REDO   , UNDO   , _______, LNE_BEG, _______, LNE_END, _______, _______,                   _______, _______,
-	_______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______),
+[_EXTEND] = LAYOUT_65_ansi_blocker(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    _______, KC_INS , KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_PGUP, PRV_WRD, KC_UP  , NXT_WRD, DEL_LNE, KC_BSPC, _______, _______,          _______,
+    _______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , KC_BSPC, _______,                   _______,
+    _______, CUT    , COPY   , PASTE  , REDO   , UNDO   , _______, LNE_BEG, _______, LNE_END, _______, _______,                   _______, _______,
+    _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______),
 
-    [_ADJUST] = LAYOUT_65_ansi_blocker(
-	RESET  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-	_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, _______, _______,          _______,
-	_______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, _______, _______, _______, _______, KC_F11 , _______, _______,                   _______,
-	_______, RGB_SAD, RGB_HUD, RGB_VAD,RGB_RMOD, _______, _______, _______, _______, _______, KC_F12 , _______,                   _______, _______,
-	_______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______)
-
+[_ADJUST] = LAYOUT_65_ansi_blocker(
+    RESET  , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______, _______, _______,          _______,
+    _______, RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD, _______, _______, _______, _______, KC_F11 , _______, _______,                   _______,
+    _______, RGB_SAD, RGB_HUD, RGB_VAD,RGB_RMOD, _______, _______, _______, _______, _______, KC_F12 , _______,                   _______, _______,
+    _______, _______, _______,                            _______,                            _______, _______,          _______, _______, _______)
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -101,61 +100,57 @@ void process_caps_word(uint16_t keycode, const keyrecord_t *record) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     process_caps_word(keycode, record);
     switch (keycode) {
-	case CAPS_WRD:
-	    // Toggle `caps_word_on`
-	    if (record->event.pressed) {
-		if (caps_word_on) {
-		    caps_word_disable();
-		} else {
-		    caps_word_enable();
-		}
-		return false;
-	    }
-	    break;
+        case CAPS_WRD:
+            // Toggle `caps_word_on`
+            if (record->event.pressed) {
+                if (caps_word_on) {
+                    caps_word_disable();
+                } else {
+                    caps_word_enable();
+                }
+                return false;
+            }
+            break;
     }
     return true;
 }
 
-// Tap dances:
-// 1. Double Q to Escape
-// 2. Double dot to issue ". <one-shot-shift>" i.e. dot, space and capitalize next letter
-
+// Double dot to issue ". <one-shot-shift>" i.e. dot, space and capitalize next letter
 void sentence_end(qk_tap_dance_state_t *state, void *user_data) {
     switch (state->count) {
-    // Double tapping TD_DOT produces
-    // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
-    // This helps to quickly end a sentence and begin another one
-    // without having to hit shift.
-    case 2:
-        /* Check that Shift is inactive */
-        if (!(get_mods() & MOD_MASK_SHIFT)) {
-            tap_code(KC_SPC);
-            /* Internal code of OSM(MOD_LSFT) */
-            add_oneshot_mods(MOD_BIT(KC_LSHIFT));
+        // Double tapping TD_DOT produces
+        // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
+        // This helps to quickly end a sentence and begin another one
+        // without having to hit shift.
+        case 2:
+            /* Check that Shift is inactive */
+            if (!(get_mods() & MOD_MASK_SHIFT)) {
+                tap_code(KC_SPC);
+                /* Internal code of OSM(MOD_LSFT) */
+                add_oneshot_mods(MOD_BIT(KC_LSHIFT));
+            } else {
+                // send ">" (KC_DOT + shift → ">")
+                tap_code(KC_DOT);
+            }
+            break;
 
-        } else {
-            // send ">" (KC_DOT + shift → ">")
+        // Since `sentence_end` is called on each tap
+        // and not at the end of the tapping term,
+        // the third tap needs to cancel the effects
+        // of the double tap in order to get the expected
+        // three dots ellipsis.
+        case 3:
+            // remove the added space of the double tap case
+            tap_code(KC_BSPC);
+            // replace the space with a second dot
             tap_code(KC_DOT);
-        }
-        break;
+            // tap the third dot
+            tap_code(KC_DOT);
+            break;
 
-    // Since `sentence_end` is called on each tap
-    // and not at the end of the tapping term,
-    // the third tap needs to cancel the effects
-    // of the double tap in order to get the expected
-    // three dots ellipsis.
-    case 3:
-        // remove the added space of the double tap case
-        tap_code(KC_BSPC);
-        // replace the space with a second dot
-        tap_code(KC_DOT);
-        // tap the third dot
-        tap_code(KC_DOT);
-        break;
-
-    // send KC_DOT on every normal tap of TD_DOT
-    default:
-        tap_code(KC_DOT);
+        // send KC_DOT on every normal tap of TD_DOT
+        default:
+            tap_code(KC_DOT);
     }
 };
 
