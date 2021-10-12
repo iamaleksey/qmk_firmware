40aa0f6e361b49c1aaec0686fa1760fc2345652a
Aleksey Yeschenko <aleksey@apache.org>
2021-10-12 11:09:42 +0100

    kyria: remove dot tap dance

--- keyboards/splitkb/kyria/keymaps/iamaleksey/keymap.c
+++ keyboards/splitkb/kyria/keymaps/iamaleksey/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAK] = LAYOUT(
     KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   ,                                     KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_BSLS,
     ESC_RSE, A_LCTL , R_LOPT , S_LCMD , T_LSFT , KC_G   ,                                     KC_M   , N_RSFT , E_RCMD , I_ROPT , O_RCTL , KC_QUOT,
-    KC_LSFT, KC_Z   , KC_X   , KC_C   , D_HYPR , KC_V   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K   , H_HYPR , KC_COMM, TD_DOT , KC_SLSH,CAPS_WRD,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , D_HYPR , KC_V   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_K   , H_HYPR , KC_COMM, KC_DOT , KC_SLSH,CAPS_WRD,
                                XXXXXXX, XXXXXXX, TAB_LWR, KC_SPC , PRV_TAB, NXT_TAB, KC_BSPC, ENT_RSE, XXXXXXX, XXXXXXX
 ),
 
@@ -245,49 +245,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
-
-// Double dot to issue ". <one-shot-shift>" i.e. dot, space and capitalize next letter
-void sentence_end(qk_tap_dance_state_t *state, void *user_data) {
-    switch (state->count) {
-        // Double tapping TD_DOT produces
-        // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
-        // This helps to quickly end a sentence and begin another one
-        // without having to hit shift.
-        case 2:
-            /* Check that Shift is inactive */
-            if (!(get_mods() & MOD_MASK_SHIFT)) {
-                tap_code(KC_SPC);
-                /* Internal code of OSM(MOD_LSFT) */
-                add_oneshot_mods(MOD_BIT(KC_LSHIFT));
-            } else {
-                // send ">" (KC_DOT + shift → ">")
-                tap_code(KC_DOT);
-            }
-            break;
-
-        // Since `sentence_end` is called on each tap
-        // and not at the end of the tapping term,
-        // the third tap needs to cancel the effects
-        // of the double tap in order to get the expected
-        // three dots ellipsis.
-        case 3:
-            // remove the added space of the double tap case
-            tap_code(KC_BSPC);
-            // replace the space with a second dot
-            tap_code(KC_DOT);
-            // tap the third dot
-            tap_code(KC_DOT);
-            break;
-
-        // send KC_DOT on every normal tap of TD_DOT
-        default:
-            tap_code(KC_DOT);
-        }
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [_TD_DOT] = ACTION_TAP_DANCE_FN_ADVANCED(sentence_end, NULL, NULL)
-};
 
 //
 // RGB layer indication
--- keyboards/splitkb/kyria/keymaps/iamaleksey/rules.mk
+++ keyboards/splitkb/kyria/keymaps/iamaleksey/rules.mk
@@ -2,4 +2,3 @@ MOUSEKEY_ENABLE    = no  # disable mouse key
 OLED_DRIVER_ENABLE = no  # disables the use of OLED displays
 ENCODER_ENABLE     = yes # enables the use of one or more encoders
 RGBLIGHT_ENABLE    = yes # enables keyboard RGB underglow
-TAP_DANCE_ENABLE   = yes # enables tap dances
