f83f770f15df1dc17f568d8d26652d5295f13bd5
Aleksey Yeschenko <aleksey@apache.org>
2021-10-22 18:03:40 +0100

    kyria: nit

--- keyboards/splitkb/kyria/keymaps/iamaleksey/keymap.c
+++ keyboards/splitkb/kyria/keymaps/iamaleksey/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
     ESC_RSE, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                               XXXXXXX, KC_LOPT, LOWER  , KC_SPC , KC_ENT , KC_ENT , KC_BSPC, ENT_RSE, KC_ROPT, XXXXXXX
+                               XXXXXXX, KC_LOPT, LOWER  , KC_SPC , KC_ENT , KC_ENT , KC_BSPC, RAISE  , KC_ROPT, XXXXXXX
 ),
 
 /*
